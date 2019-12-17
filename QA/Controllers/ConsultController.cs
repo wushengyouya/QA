1319f0129e486131713db8a53adcc81bb40d483d
mikasa <1009889421@qq.com>
2019-12-17 19:05:18 +0800

    数据库课程设计

---
+++ QA/Controllers/ConsultController.cs
@@ -0,0 +1,21 @@
+﻿using QA.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace QA.Controllers
+{
+    public class ConsultController : Controller
+    {
+        // GET: Index
+        public ActionResult Index()
+        {
+            OnlineQEntities onlineQEntities = new OnlineQEntities();
+            IList<Doctor> doctors = onlineQEntities.Doctors.ToList();
+
+            return View(doctors);
+        }
+    }
+}
---
+++ QA/Controllers/UserController.cs
@@ -0,0 +1,22 @@
+﻿    using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace QA.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: Login
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        public ActionResult Personal()
+        {
+            return View();
+        }
+    }
+}
