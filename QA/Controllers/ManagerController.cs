fd7409021cb07c18810a5ac79cacfbec1af7db5b
mikasa <1009889421@qq.com>
2019-12-18 15:28:58 +0800

    编写登录模块,管理员模块

---
+++ QA/Controllers/ManagerController.cs
@@ -0,0 +1,72 @@
+﻿using QA.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace QA.Controllers
+{
+    public class ManagerController : Controller
+    {
+        private OnlineQEntities onlineQEntities = new OnlineQEntities();
+        // GET: Manager
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// 添加科室
+        /// </summary>
+        /// <param name="section"></param>
+        /// <returns></returns>
+        public ActionResult AddSection(Section section)
+        {
+            onlineQEntities.Sections.Add(section);
+            SaveChanges();
+
+            //跳转至所有科室界面
+            return View("Section");
+        }
+        
+        public  ActionResult UpdateSection(Section section)
+        {
+            onlineQEntities.Entry(section);
+            SaveChanges();
+
+            //刷新当前页面
+            return View();
+        }
+
+
+        public ActionResult AddDoctor(Doctor doctor)
+        {
+            onlineQEntities.Doctors.Add(doctor);
+            SaveChanges();
+
+            //跳转至所有医生界面
+            return View();
+        }
+        
+        public ActionResult UpdateDoctor(Doctor doctor)
+        {
+            onlineQEntities.Entry(doctor);
+            SaveChanges();
+
+            //刷新当前修改医生信息的界面
+            return View();
+        }
+
+
+
+        /// <summary>
+        /// 保存修改
+        /// </summary>
+        /// <returns></returns>
+        public int SaveChanges()
+        {
+            return onlineQEntities.SaveChanges();
+        }
+    }
+}
--- QA/Controllers/UserController.cs
+++ QA/Controllers/UserController.cs
@@ -11,14 +11,76 @@ namespace QA.Controllers
     {
         private OnlineQEntities onlineQEntities = new OnlineQEntities();
         // GET: Login
-        public ActionResult Login()
+
+        /// <summary>
+        /// 用户登录
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Login(int flag,string userAccount,string userPwd)
         {
-            return View();
+            //病人登录
+            if (flag==0)
+            {
+                var patientInfo = onlineQEntities.Patients.FirstOrDefault(p => p.p_account.Equals(userAccount));
+                if(patientInfo !=null && patientInfo.p_account.Equals(userAccount) && patientInfo.Equals(userPwd))
+                {
+                    //跳转至选择科室界面,目前展示的是个人信息界面
+                    return View("PatientPersonal");
+                }
+                   
+            }
+
+            //医生登录
+            var doctorInfo = onlineQEntities.Doctors.FirstOrDefault(d => d.d_account.Equals(userAccount);)
+            if(doctorInfo!=null && doctorInfo.d_account.Equals(userAccount) && doctorInfo.password.Equals(userPwd))
+            {
+                //跳转至回答咨询界面
+                return View("DoctorPersonal");
+            }
+            return Content("密码或用户名错误");
         }
 
-        public ActionResult Personal()
+        /// <summary>
+        /// 患者个人信息
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public ActionResult PatientPersonal(string id)
         {
-            return View();
+            var patient = onlineQEntities.Patients.First(p => p.ID == id);
+            return View(patient);
+        }
+
+        /// <summary>
+        /// 医生个人信息
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public ActionResult DoctorPersonal(string id)
+        {
+            var doctor = onlineQEntities.Doctors.First(d => d.Id == id);
+            return View(doctor);
+        }
+
+        /// <summary>
+        /// 修改患者信息
+        /// </summary>
+        /// <param name="doctor"></param>
+        /// <returns></returns>
+        public ActionResult UpdatePatientInfo(Doctor doctor)
+        {
+            onlineQEntities.Entry(doctor);
+            SaveChanges();
+            return View("PatientPersonal");
+        }
+
+        /// <summary>
+        /// 保存修改
+        /// </summary>
+        /// <returns></returns>
+        public int SaveChanges()
+        {
+           return  onlineQEntities.SaveChanges();
         }
     }
 }
